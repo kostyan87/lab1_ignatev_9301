6647b5612f24cd1f0d3513f80575165dd97463c7
kostyan <kostya.ignatev.14@mail.ru>
2020-11-08 14:54:00 +0300

    Удалено поле индексов и откорректирована работа с ними, исправлено использование throw

--- tests/list_for_tests.js
+++ tests/list_for_tests.js
@@ -1,7 +1,6 @@
 class Node {
    constructor(value) {
       this.value = value;
-      this.index = 0;
       this.next = null;
       this.previous = null;
    }
@@ -34,7 +33,6 @@ class DoublyLinkedList {
          newNode.previous = this.tail;
          this.tail = newNode;
          this.length = this.length + 1;
-         newNode.index = newNode.previous.index + 1;
          newNode.next = null;
       }
    }
@@ -42,33 +40,15 @@ class DoublyLinkedList {
    at(initial_index) {
       if (initial_index < 0 || initial_index > this.get_size() - 1 || typeof initial_index != 'number') {
          throw 'There is no element with this index in the list';
-      } else {
-         let listElem = this.head;
-         while (listElem.index != initial_index) {
-            listElem = listElem.next;
-         }
-         return listElem;
       }
-   }
-
-   //increases all indexes by one
-   increases_indexes(initial_index) {
-      let listElem = this.at(initial_index).next;
-
-      for (let i = initial_index; i < this.get_size() - 1; i++) {
-         listElem.index++;
+      let listElem = this.head;
+      let i = 0
+      while (i != initial_index) {
+         if (i == initial_index) break;
+         i++;
          listElem = listElem.next;
       }
-   }
-
-   //reduces all indexes by one
-   reduces_indexes(index) {
-      let listElem = this.at(index);
-
-      for (let i = index; i < this.get_size() + 1; i++) {
-         listElem.index--;
-         listElem = listElem.next;
-      }
+      return listElem;
    }
 
    push_front(value) {
@@ -82,7 +62,6 @@ class DoublyLinkedList {
          newNode.next = this.head;
          this.head = newNode;
          this.length = this.length + 1;
-         this.increases_indexes(0);
       }
    }
 
@@ -94,8 +73,8 @@ class DoublyLinkedList {
          let listArray = '';
 
          for (let i = 0; i < this.get_size(); i++) {
-            if (i == this.get_size() - 1) listArray = listArray + '[' + listElem.index + ']' + listElem.value;
-            else listArray = listArray + '[' + listElem.index + ']' + listElem.value + separator;
+            if (i == this.get_size() - 1) listArray = listArray + listElem.value;
+            else listArray = listArray + listElem.value + separator;
             listElem = listElem.next;
          }
 
@@ -106,125 +85,109 @@ class DoublyLinkedList {
    pop_back() {
       if (this.isEmpty()) {
          throw 'The list is empty';
+      }
+      if (this.get_size() == 1) {
+         this.head = null;
+         this.tail = null;
+         this.length--;
       } else {
-         if (this.get_size() == 1) {
-            this.head = null;
-            this.tail = null;
-            this.length--;
-         } else {
-            this.tail.previous.next = null;
-            this.tail = this.tail.previous;
-            this.length--;
-         }
+         this.tail.previous.next = null;
+         this.tail = this.tail.previous;
+         this.length--;
       }
    }
 
    pop_front() {
       if (this.isEmpty()) {
          throw 'The list is empty';
+      }
+      if (this.get_size() == 1) {
+         this.head = null;
+         this.tail = null;
+         this.length--;
       } else {
-         if (this.get_size() == 1) {
-            this.head = null;
-            this.tail = null;
-            this.length--;
-         } else {
-            this.head.next.previous = null;
-            this.head = this.head.next;
-            this.length--;
-            if (this.head.index == this.get_size()) {
-               this.head.index--;
-            } else {
-               this.reduces_indexes(1);
-            }
-         }
+         this.head.next.previous = null;
+         this.head = this.head.next;
+         this.length--;
       }
    }
 
    insert(value, index) {
       if (this.isEmpty()) {
          throw 'The list is empty';
-      } else if (index < -1 || index > this.get_size() - 1 || typeof index != 'number') {
+      }
+      if (index < -1 || index > this.get_size() - 1 || typeof index != 'number') {
          throw 'There is no element with this index in the list';
+      }
+      let newItem = new Node(value);
+
+      if (index == -1) {
+         this.push_front(value);
+      } else if (index == this.tail.index) {
+         this.push_back(value);
       } else {
-         let newItem = new Node(value);
+         let previousItem = this.at(index);
 
-         if (index == -1) {
-            this.push_front(value);
-         } else if (index == this.tail.index) {
-            this.push_back(value);
-         } else {
-            let previousItem = this.at(index);
-
-            newItem.next = previousItem.next;
-            newItem.previous = previousItem;
-            previousItem.next.previous = newItem;
-            previousItem.next = newItem;
-            this.length++;
-            newItem.index = previousItem.index + 1;
-            this.increases_indexes(newItem.index);
-         }
+         newItem.next = previousItem.next;
+         newItem.previous = previousItem;
+         previousItem.next.previous = newItem;
+         previousItem.next = newItem;
+         this.length++;
       }
    }
 
    remove(index) {
       if (this.isEmpty()) {
          throw 'The list is empty';
-      } else if (index < 0 || index > this.get_size() - 1 || typeof index != 'number') {
+      }
+      if (index < 0 || index > this.get_size() - 1 || typeof index != 'number') {
          throw 'There is no element with this index in the list';
+      }
+      let removeElem = this.at(index);
+      if (index == 0) {
+         this.pop_front();
+      } else if (index == this.get_size() - 1) {
+         this.pop_back();
       } else {
-         let removeElem = this.at(index);
-         if (index == 0) {
-            this.pop_front();
-         } else if (index == this.get_size() - 1) {
-            this.pop_back();
-         } else {
-            removeElem.previous.next = removeElem.next;
-            removeElem.next.previous = removeElem.previous;
-            this.length--;
-            if (removeElem.next.index == this.get_size()) {
-               removeElem.next.index--;
-            } else {
-               this.reduces_indexes(removeElem.next.index);
-            }
-         }
+         removeElem.previous.next = removeElem.next;
+         removeElem.next.previous = removeElem.previous;
+         this.length--;
       }
    }
 
    set(value, index) {
       if (this.isEmpty()) {
          throw 'The list is empty';
-      } else if (index < 0 || index > this.get_size() - 1 || typeof index != 'number') {
-         throw 'There is no element with this index in the list';
-      } else {
-         this.remove(index);
-         this.insert(value, index - 1);
       }
+      if (index < 0 || index > this.get_size() - 1 || typeof index != 'number') {
+         throw 'There is no element with this index in the list';
+      }
+      this.remove(index);
+      this.insert(value, index - 1);
    }
 
    clear() {
       if (this.isEmpty()) {
          throw 'The list is empty';
-      } else {
-         const initialListSize = this.get_size();
-         for (let i = 0; i < initialListSize; i++) {
-            this.remove(0);
-         }
+      }
+      const initialListSize = this.get_size();
+      for (let i = 0; i < initialListSize; i++) {
+         this.remove(0);
       }
    }
 
    push_front_list(list) {
       if (typeof list != 'object') {
          throw 'Invalid data type';
-      } else {
-         if (list.isEmpty()) {
-            throw 'Added sheet is empty';
-         }
-         let listElem = list.tail;
+      }
+      if (list.isEmpty()) {
+         throw 'Added sheet is empty';
+      }
+      let listElem = list.tail;
 
-         for (let i = list.get_size() - 1; i >= 0; i--) {
-            this.push_front(listElem.value);
-            listElem = listElem.previous;
-         }
+      for (let i = list.get_size() - 1; i >= 0; i--) {
+         this.push_front(listElem.value);
+         listElem = listElem.previous;
       }
    }
 }
